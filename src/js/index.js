a3383ed1832bb142cf038ec39fe9bdec7b0cd8d0
PixelPioneer404 <rajbeersaha.me@gmail.com>
2025-06-05 23:43:05 +0530

    created the works section cards completely and also the easter egg in the hero section

--- src/js/index.js
+++ src/js/index.js
@@ -19,14 +19,14 @@ const totalWidth = wrapper.scrollWidth;
 const scrollDistance = totalWidth - visibleWidth;
 
 // 5) give page enough vertical scroll space
-document.body.style.height = `${scrollDistance + window.innerHeight}px`;
+// document.body.style.height = `${scrollDistance + window.innerHeight}px`;
 
 // 6) animate the wrapper instead of the panels array
 gsap.to(wrapper, {
     x: () => `-${scrollDistance}px`,
     ease: "none",
     scrollTrigger: {
-        trigger: section,
+        trigger: ".worksSection",
         start: "top top",
         end: () => `+=${scrollDistance}`,
         scrub: true,
@@ -34,3 +34,60 @@ gsap.to(wrapper, {
         pinSpacing: true
     }
 });
+
+const workCardSpans = document.querySelectorAll(".work-card-span");
+
+workCardSpans.forEach((expressJsSpan) => {
+    const workCardSpanSvgPath = expressJsSpan.querySelector("svg path");
+
+    expressJsSpan.addEventListener("mouseenter", () => {
+        workCardSpanSvgPath.setAttribute("fill", "#191923");
+    });
+
+    expressJsSpan.addEventListener("mouseleave", () => {
+        workCardSpanSvgPath.setAttribute("fill", "#edede9");
+    });
+})
+
+var path = `M 0 48 Q 48 48 96 48`
+var finalPath = `M 0 48 Q 48 48 96 48`
+var stringContainer = document.querySelector("#my-name-string")
+var stringPath = stringContainer.querySelector("svg path")
+
+let isActive = false
+
+stringPath.addEventListener("mouseenter", () => {
+    isActive = true
+})
+
+stringContainer.addEventListener("mousemove", (dets) => {
+    var actualHeight = dets.clientY - stringContainer.getBoundingClientRect().top;
+    var actualWidth = dets.clientX - stringContainer.getBoundingClientRect().left;
+
+    if (isActive) {
+        path = `M 0 48 Q ${actualWidth} ${actualHeight} 96 48`
+        gsap.to("#my-name-string svg path", {
+            attr: { d: path },
+            duration: 0.3
+        })
+    }
+})
+
+stringContainer.addEventListener("mouseleave", () => {
+    isActive = false
+    gsap.to("#my-name-string svg path", {
+        attr: { d: finalPath },
+        duration: 0.3,
+        ease: "elastic.out(1, 0.3)"
+    })
+})
+
+const scrollDownCueBtn = document.querySelectorAll(".scroll-to-work")
+
+scrollDownCueBtn.forEach((el) => {
+    el.addEventListener("click", () => {
+        document.querySelector("#works").scrollIntoView({
+            behavior: "smooth"
+        })
+    })
+})
