df69721416c8e4a1bdaeba205cdc299525ee4de6
PixelPioneer404 <rajbeersaha.me@gmail.com>
2025-06-06 22:37:11 +0530

    created the show more projects morph and other micro interactions and entrance animations

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,14 @@
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-gsap.registerPlugin(ScrollTrigger);
+import { gsap } from "gsap"
+import { ScrollTrigger } from "gsap/ScrollTrigger"
+import { MorphSVGPlugin } from "gsap/MorphSVGPlugin"
+gsap.registerPlugin(ScrollTrigger, MorphSVGPlugin)
+
+window.onload = () => {
+    // animateNav()
+    // animateSocialLabels()
+    updateStatsOnLoad()
+    // btnEnters()
+}
 
 // 1) grab section + wrapper
 const section = document.querySelector(".worksSection");
@@ -82,7 +90,7 @@ stringContainer.addEventListener("mouseleave", () => {
     })
 })
 
-const scrollDownCueBtn = document.querySelectorAll(".scroll-to-work")
+const scrollDownCueBtn = document.querySelectorAll(".scroll-to-work") //for the scroll-cue and work nav-item
 
 scrollDownCueBtn.forEach((el) => {
     el.addEventListener("click", () => {
@@ -91,3 +99,174 @@ scrollDownCueBtn.forEach((el) => {
         })
     })
 })
+
+const yDist = -25
+const yDur = 0.3
+
+const socials = [
+    { textSel: "#linkedin-text", iconSel: "#linkedin-icon" },
+    { textSel: "#instagram-text", iconSel: "#instagram-icon" },
+    { textSel: "#github-text", iconSel: "#github-icon" }
+]
+
+socials.forEach(({ textSel, iconSel }) => {
+    const textEl = document.querySelector(textSel)
+    const iconEl = document.querySelector(iconSel)
+
+    textEl.addEventListener("mouseenter", () => {
+        gsap.to(iconEl, {
+            opacity: 1,
+            y: yDist,
+            duration: yDur
+        })
+    })
+
+    textEl.addEventListener("mouseleave", () => {
+        gsap.to(iconEl, {
+            opacity: 0,
+            y: 0,
+            duration: yDur
+        })
+    })
+})
+
+const projectsCount = document.querySelector("#completed-projects-count")
+const clientsCount = document.querySelector("#clients-count")
+function updateStatsOnLoad() {
+    gsap.to(projectsCount, {
+        innerText: 200,
+        duration: 1.5,
+        snap: {
+            innerText: 2
+        }
+    })
+
+    gsap.to(clientsCount, {
+        innerText: 40,
+        duration: 1.5,
+        snap: "innerText"
+    })
+}
+
+// window.onload = updateStatsOnLoad (go to line 233)
+
+const seeMoreProjectsLabel = document.querySelector("#see-more-projects-label")
+const arrowContainer = document.querySelector("#arrow-svg-container")
+
+const narrowArrowPath = `M504-480 320-664l56-56 240 240-240 240-56-56 184-184Z`
+const wideArrowPath = `M504 -480 L320 -712 L376 -768 L616 -480 L376 -192 L320 -248 Z`
+
+seeMoreProjectsLabel.addEventListener("mouseenter", () => {
+    gsap.to(arrowContainer, { x: 4, duration: 0.3 })
+    gsap.to("#arrow-svg-container path", { morphSVG: wideArrowPath, duration: 0.3, ease: "power1.inOut" })
+})
+
+seeMoreProjectsLabel.addEventListener("mouseleave", () => {
+    gsap.to(arrowContainer, { x: 0, duration: 0.3 })
+    gsap.to("#arrow-svg-container path", { morphSVG: narrowArrowPath, duration: 0.3, ease: "power1.inOut" })
+})
+
+const scrollCueBtn = document.querySelector("#scroll-cue-btn")
+
+scrollCueBtn.addEventListener("mouseenter", () => {
+    gsap.to("#scroll-cue-btn svg", {
+        rotate: 90,
+        duration: 0.3,
+        ease: "power1.inOut"
+    })
+})
+
+scrollCueBtn.addEventListener("mouseleave", () => {
+    gsap.to("#scroll-cue-btn svg", {
+        rotate: -45,
+        duration: 0.3,
+        ease: "power1.inOut"
+    })
+})
+
+function btnEnters() { //go to line 233
+    gsap.from("#scroll-cue-btn", {
+        opacity: 0,
+        duration: 1.6,
+    })
+}
+
+const DESIGNERtxtContainer = document.querySelector("#DESIGNER-txt-container")
+var DesignerTxt = DESIGNERtxtContainer.innerText
+var eachEl1 = DesignerTxt.split("")
+var clutter1 = ""
+
+const DEVELOPERtxtContainer = document.querySelector("#DEVELOPER-txt-container")
+var DeveloperTxt = DEVELOPERtxtContainer.innerText
+var eachEl2 = DeveloperTxt.split("")
+var clutter2 = ""
+
+DESIGNERtxtContainer.textContent = ""
+eachEl1.forEach((el, i) => {
+    if (i === 2) {
+        clutter1 += `<span class="inline-block font-clash-display font-bold text-[120px] text-stroke">${el}</span>`
+    } else {
+        clutter1 += `<span>${el}</span>`
+    }
+    DESIGNERtxtContainer.innerHTML = clutter1
+})
+
+gsap.from("#DESIGNER-txt-container span", {
+    y: 120,
+    opacity: 0,
+    duration: 0.5,
+    stagger: 0.1
+})
+
+DEVELOPERtxtContainer.textContent = ""
+eachEl2.forEach((el, i) => {
+    if (i === 8) {
+        clutter2 += `<span class="inline-block font-clash-display font-bold text-[150px] text-stroke">${el}</span>`
+    } else {
+        clutter2 += `<span>${el}</span>`
+    }
+    DEVELOPERtxtContainer.innerHTML = clutter2
+})
+
+gsap.from("#DEVELOPER-txt-container span", {
+    y: 120,
+    opacity: 0,
+    duration: 0.5,
+    stagger: 0.1
+})
+
+const socialLabels = document.querySelectorAll(".social-label")
+function animateSocialLabels() {
+    gsap.from(socialLabels, {
+        translateY: "200%",
+        opacity: 0,
+        duration: 0.5,
+        stagger: {
+            each: 0.2,
+            from: "end"   // ← now it will animate indexes [2,1,0] instead of [0,1,2]
+        }
+    });
+}
+
+const navName = document.querySelector("#nav-name")
+const navItems = document.querySelectorAll(".nav-item")
+const navBtn = document.querySelector("#resume")
+
+function animateNav() {
+    gsap.from(navName, {
+        left: "-10%",
+        duration: 1,
+        ease: "power1.inOut",
+    })
+    gsap.from(navBtn, {
+        right: "-10%",
+        duration: 1,
+        ease: "power1.inOut",
+    })
+    gsap.from(navItems, {
+        yPercent: -200,
+        opacity: 0,
+        duration: 0.5,
+        stagger: 0.3
+    })
+}
